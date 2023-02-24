0ff2041f6069831065bee01d055ae4a3d2fa2e46
CHO-O <327cjm@gmail.com>
2023-02-24 10:21:55 +0900

    react-async로 요청 상태 관리

--- src/User.js
+++ src/User.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import axios from 'axios';
-import useAsync from './useAsync';
+import {useAsync} from 'react-async';
 
-async function getUser(id) {
+async function getUser({id}) { //프로미스를 반환하는 함수의 파라미터를 객체 형태로 주어야 함
     const response = await axios.get(
         `https://jsonplaceholder.typicode.com/users/${id}`
     );
@@ -10,10 +10,13 @@ async function getUser(id) {
 }
 
 function User({ id }){
-    const [state] = useAsync(() => getUser(id), [id]);
-    const { loading, data: user, error } = state;
+    const { data:user, error, isLoading } = useAsync({
+        promiseFn: getUser,
+        id,
+        watch: id
+    });
 
-    if(loading) return <div>로딩중..</div>;
+    if(isLoading) return <div>로딩중..</div>;
     if(error) return <div>에러 발생</div>;
     if(!user) return null;
 
@@ -28,3 +31,7 @@ function User({ id }){
 }
 
 export default User;
+
+//react-async는 useAsync와 비슷한 함수가 들어있는 라이브러리 -> 직접 요청 상태 관리를 위한 커스텀 Hook을 만들기 귀찮을 때 사용
+//이 라이브러리 안의 함수 이름도 useAsync긴 한데 사용법이 다름
+//만들었던 커스텀 Hook은 결과물을 '배열'로 반환 / react-async의 Hook은 '객체 형태'로 반환
--- src/Users.js
+++ src/Users.js
@@ -1,6 +1,6 @@
-import React, {useState, useReducer, useEffect} from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
-import useAsync from './useAsync';
+import { useAsync } from 'react-async';
 import User from './User';
 // useAsync에서는 Promise의 결과를 바로 data에 담기 때문에
 // 요청을 한 이후 response에서 data 추출하여 반환하는 함수를 하단에 따로 만들었다
@@ -13,13 +13,15 @@ async function getUsers(){
 
 function Users(){
     const [userId, setUserId] = useState(null);
-    const [state, refetch] = useAsync(getUsers, [], true);
+    const {data:users, error, isLoading, run} = useAsync({
+        deferFn: getUsers
+    });
 
-    const {loading, data: users, error} = state; //state.data 를 users 키워드로 조회
+    // const {loading, data: users, error} = state; //state.data 를 users 키워드로 조회
     
-    if(loading) return <div>로딩중...</div>
+    if(isLoading) return <div>로딩중...</div>
     if(error) return <div>에러 발생</div>
-    if(!users) return <button onClick={refetch}>불러오기</button>; //users값이 아직 없을 땐 null을 보여주도록 처리 , !users는 users값이 undefined이거나 null일 때를 뜻함
+    if(!users) return <button onClick={run}>불러오기</button>; //users값이 아직 없을 땐 null을 보여주도록 처리 , !users는 users값이 undefined이거나 null일 때를 뜻함
     return(
         <>
             <ul> 
@@ -33,7 +35,7 @@ function Users(){
                     </li>
                 ))}
             </ul>
-            <button onClick={refetch}>다시 불러오기</button>
+            <button onClick={run}>다시 불러오기</button>
             {userId && <User id={userId} />}
         </>
         //ul > unordered list
@@ -41,3 +43,7 @@ function Users(){
 }
 
 export default Users;
+
+//refetch 대신 reload
+//skip처럼 렌더링하는 시점이 아니라 사용자의 특정 인터랙션에 따라 API를 호출하고 싶을 땐
+//promiseFn > deferFn, reload > run
